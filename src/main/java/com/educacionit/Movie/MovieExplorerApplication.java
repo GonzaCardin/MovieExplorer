0af70933c6a300667a062720b699d147b1882345
Gonzalo Cardin <gonzalocardin@gmail.com>
2024-07-12 15:21:17 -0300

    ..

--- src/main/java/com/educacionit/Movie/MovieExplorerApplication.java
+++ src/main/java/com/educacionit/Movie/MovieExplorerApplication.java
@@ -1,4 +1,4 @@
-package com.educacionit;
+package com.educacionit.Movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/educacionit/Movie/MovieExplorerApplicationTests.java
+++ src/test/java/com/educacionit/Movie/MovieExplorerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.educacionit;
+package com.educacionit.Movie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
